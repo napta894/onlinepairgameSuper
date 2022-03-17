cf54ef58cba877bf319bd2c2f3af2d13982e8953
Napta Hag <naptahag@gmail.com>
2022-03-17 11:02:51 +0000

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,33 @@
+use = "strict";
 
 const quizContainer = document.getElementById('quiz');
 const resultsContainer = document.getElementById('results');
 const submitButton = document.getElementById('submit');
+
+
+function buildQuiz() { }
+
+function showResults() { }
+
+// display quiz right away
+buildQuiz();
+
+// on submit, show results
+submitButton.addEventListener('click', showResults);
+
+
+// Display Questions
+
+const myQuestions = [
+    {
+        question: "What is the capital of France?",
+        answers: {
+            a: "London",
+            b: "Paris",
+            c: "Rome",
+            d: "Berlin"
+        },
+        correctAnswer: "b"
+    },
+    
+];
