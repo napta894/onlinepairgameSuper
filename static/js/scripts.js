fdec33adca00dd265147c8b2480376bc480efb32
Napta Hag <naptahag@gmail.com>
2022-03-31 16:02:38 +0100

    editing the quiz

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -1,5 +1,6 @@
 use = "strict";
 
+//*
 const quizContainer = document.getElementById('quiz');
 const resultsContainer = document.getElementById('results');
 const submitButton = document.getElementById('submit');
@@ -10,24 +11,161 @@ function buildQuiz() { }
 function showResults() { }
 
 // display quiz right away
+
 buildQuiz();
 
 // on submit, show results
-submitButton.addEventListener('click', showResults);
 
 
 // Display Questions
 
-const myQuestions = [
-    {
-        question: "What is the capital of France?",
-        answers: {
-            a: "London",
-            b: "Paris",
-            c: "Rome",
-            d: "Berlin"
+let questions = 
+
+    [
+        {
+            "id": 1,
+            "question": "Number(\"1\") - 1 == 0; \rWhat is the result?",
+            "a": "True",
+            "b": "False",
+            "c": "NaN",
+            "d": "TypeError",
+            "answer": "a"
         },
-        correctAnswer: "b"
-    },
+        {
+            "id": 2,
+            "question": "What is the result? \r(true + false) > 2 + true;",
+            "a": "true",
+            "b": "false",
+            "c": "TypeError",
+            "d": "NaN",
+            "answer": "2"
+        },
+        {
+            "id": 3,
+            "question": "What is the result?\r\"1\" - - \"1\";",
+            "a": "0",
+            "b": "2",
+            "c": "11",
+            "d": "\"11\"",
+            "answer": "b"
+        },
+        {
+            "id": 4,
+            "question": "What is the result? \r new String(\"This is a string\") instanceof String;        ",
+            "a": "true",
+            "b": "false",
+            "c": "TypeError",
+            "d": "NaN",
+            "answer": "a"
+        },
+        {
+            "id": 5,
+            "question": "What is the result? \r[] + [] + 'foo'.split('');        ",
+            "a": "\"f,o,o\"",
+            "b": "TypeError",
+            "c": "[\"f\",\"o\",\"o\"]",
+            "d": "[][][\"f\",\"o\",\"o\"]",
+            "answer": "a"
+        },
+        {
+            "id": 6,
+            "question": "What is the result? \r[] + [] + 'foo'.split('');",
+            "a": "\"f, o, o\"",
+            "b": "TypeError",
+            "c": "[\"f\", \"o\", \"o\"]",
+            "d": "[][][\"f\", \"o\", \"o\"]",
+            "answer": "xxx"
+        },
+        {
+            "id": 7,
+            "question": "What is the result? \rnew Array(5).toString();",
+            "a": "\",,,,\"",
+            "b": "[]",
+            "c": "\"[]\"",
+            "d": "(empty)",
+            "answer": "a"
+        },
+        {
+            "id": 8,
+            "question": "What is the result? \rString('Hello') === 'Hello';",
+            "a": "true",
+            "b": "false",
+            "c": "TypeError",
+            "d": "NaN",
+            "answer": "a"
+        },
+        {
+            "id": 1111,
+            "question": "What is the result? \r\"This is a string\" instanceof String;",
+            "a": "true",
+            "b": "false",
+            "c": "TypeError",
+            "d": "NaN",
+            "answer": "xxx"
+        },
+        {
+            "id": 1111,
+            "question": "What is the result console.log(myArr); ? \rvar myArr = ['foo', 'bar', 'baz']; \rmyArr.length = 0; \r myArr.push('bin');",
+            "a": "['foo', 'bar', 'baz']",
+            "b": "['foo', 'bar', 'baz', 'bin']",
+            "c": "['bin', 'foo', 'bar', 'baz']",
+            "d": "['bin']",
+            "answer": "d"
+        },
+        {
+            "id": 1111,
+            "question": "What is the result? \rString('Hello') === 'Hello';",
+            "a": "true",
+            "b": "false",
+            "c": "TypeError",
+            "d": "NaN",
+            "answer": "a"
+        },
+        {
+            "id": 1111,
+            "question": "What is the result? \r10 > 9 > 8 === true;",
+            "a": "True",
+            "b": "False",
+            "c": "Error",
+            "d": "Other",
+            "answer": "b"
+        },
+        {
+            "id": 1111,
+            "question": "What is the result? \rNaN === NaN;",
+            "a": "true",
+            "b": "false",
+            "c": "TypeError",
+            "d": "NaN",
+            "answer": "b"
+        }
+    ]
+const question = document.getElementById("questions");
+const answers = Array.from(document.getElementbyClassName("answer-text"));
+const questionCounterText = document.getElementById("counter");
+const scoreText = document.getElementById("score");
+
     
-];
+console.log(questions);
+
+let questionCounter;
+let score;
+let MAX_QUESTIONS = 3;
+
+startGame = () => {
+    questionCounter= 0;
+    score = 0;
+    AvailableQuetions = getRandomQuestions(questions, MAX_QUESTIONS);
+}
+
+const getRandomQuestions = (arr, n) => {
+
+    Let len = arr.lenght; 
+    if (n > len) {
+        throw new RangeError(
+            
+        )
+    }
+ };
+
+
